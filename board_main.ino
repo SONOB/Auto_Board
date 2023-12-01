d03647149324539e3e22be5b4bf8f3b8ec356287
이용진 <118358452+SONOB@users.noreply.github.com>
2023-12-01 12:16:10 +0900

    board_main.ino

--- board_main.ino
+++ board_main.ino
@@ -3,12 +3,10 @@
 #include <RF24.h>
 #include <Servo.h>
 #define SPMAX 90 //모터 최대출력값 지정 BLDC모터이기 때문에 75도 상당히 위험함 따라서 낮춰쓰는것을 추천
-
 Servo motor1,motor2;
 int val1 = 60; //왼쪽바퀴 기본 모터값 설정
 int val2 = 60; //오른쪽바퀴 기본 모터값 설정
 RF24 radio(7, 8);
-
 const byte address[6] = "02019"; //송신기와 동일하게 설정 컨트롤러에도 02019로 설정되 있을 것임
 struct Data{
   byte joyX;
@@ -40,11 +38,23 @@ void loop() {
       val2 = 60;
     }
     else{
-      if(data.joyX < 10){ // 조이스틱 방향따라서 속도를 4단계로 나눠둠
+      if(data.joyX < 10){
         val1 = 70;
         val2 = 70;
       }
-      if(dat지
+      if(data.joyX > 169){
+        val1 = 65;
+        val2 = 65;
+      }
+      if(data.joyY > 169){ 
+        val1 = 75;
+        val2 = 75;
+      }
+      if(data.joyY < 10){ 
+        val1 = 80;
+        val2 = 80;
+      }
+      if(data.joyZ == 0){
         val1 = 60;
         val2 = 60;
       }
